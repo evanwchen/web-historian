7869a59969fbd6d6a5cb14e400c20512f8cdfe37
evanwchen <evanwchen@gmail.com>
2016-02-03 15:55:28 -0800

    Handle 'GET' request for index.html and pass first test

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,12 +10,33 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
-exports.serveAssets = function(res, asset, callback) {
+exports.serveAssets = function(response, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+  fs.readFile(archive.paths.siteAssets + asset, function(err, data){
+    if (err) throw err;
+    callback(data);
+  });
+
 };
 
+exports.sendResponse = function(response, data, statusCode) {
+  statusCode = statusCode || 200;
+  response.writeHead(statusCode, headers);
+  response.end(data);
+};
+
+exports.makeActionHandler = function(actionMap) {
+  return function(request, response) {
+    var action = actionMap[request.method];
+    if (action) {
+      action(request, response);
+    } else {
+      exports.sendResponse(response, '', 404);
+    }
+  };
+};
 
 
 // As you progress, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,33 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+var fs = require('fs');
+
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+var actions = {
+  'GET': function(request, response, pathname) {
+    if (request.url === '/') {
+      request.url = '/index.html';
+      httpHelpers.serveAssets(response, request.url, function(content) {
+        httpHelpers.sendResponse(response, content, 200);
+      });
+    }
+    // response.writeHead(200, httpHelpers.headers);
+    // var data = fs.readFileSync(archive.paths.siteAssets);
+    // response.end(JSON.stringify(data));
+  },
+  'POST': function(request, response) {
+
+  },
+  'OPTIONS': function(request, response) {
+
+  }
 };
+
+exports.handleRequest = httpHelpers.makeActionHandler(actions);
+
+
+// exports.handleRequest = function (req, res) {
+
+// };
